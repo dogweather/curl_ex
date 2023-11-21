1c0e1e67679f6259555964dbc81824060656c378
Robb Shecter <robb@public.law>
2023-11-21 14:19:57 -0700

    failing test

--- test/curl_ex_test.exs
+++ test/curl_ex_test.exs
@@ -9,4 +9,9 @@ defmodule CurlExTest do
   test "get! returns the contents of an http URL" do
     assert CurlEx.get!("http://neverssl.com") =~ "NeverSSL"
   end
+
+  test "get! returns the contents of an http URL using a custom User Agent" do
+    web_page_that_checks_user_agent = "https://www.axios.com/local/portland/2023/11/20/ppb-police-body-came-explained-how-it-works"
+    assert CurlEx.get!(web_page_that_checks_user_agent, user_agent: :microsoft_edge_windows) =~ "agencies can't use facial recognition software"
+  end
 end
